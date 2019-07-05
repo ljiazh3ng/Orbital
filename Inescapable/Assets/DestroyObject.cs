e5a5307aba10a1d7945a279d5306693bd38a552b
Jia Zheng <ljiazh3ng@gmail.com>
2019-07-05 17:37:41 +0800

    Revert "Added highscore table, edited torchlight and skybox"
    This reverts commit 879a3156a8a69ce08ebfaae536b8145cd416aff2.

---
+++ Inescapable/Assets/DestroyObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
