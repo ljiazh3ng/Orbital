378dca2ff057cce516c9c49c81a0509a156b680b
Jia Zheng <ljiazh3ng@gmail.com>
2019-07-05 18:16:57 +0800

    Light ,SkyBox ,HighScore

--- Inescapable/Assets/Scenes/MainMenu.cs
+++ Inescapable/Assets/Scenes/MainMenu.cs
@@ -9,7 +9,11 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+    public void HighScore()
+    {
+        SceneManager.LoadScene(2);
 
+        }
     public void QuitGame()
     {
         Application.Quit();
