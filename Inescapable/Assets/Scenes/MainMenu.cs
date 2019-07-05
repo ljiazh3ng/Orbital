bf3254b468368a22df74313c2066614f81991176
XinWei <zhouxinwei97@gmail.com>
2019-07-05 23:44:35 +0800

    HighScore table tab
    Added Highscore Table tab

--- Inescapable/Assets/Scenes/MainMenu.cs
+++ Inescapable/Assets/Scenes/MainMenu.cs
@@ -10,6 +10,12 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+    public void HighScore()
+    {
+        SceneManager.LoadScene(2);
+
+    }
+
     public void QuitGame()
     {
         Application.Quit();
