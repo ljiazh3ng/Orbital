92b68ec172598a973d3f6a1bd3e955a87c75b7cf
Jia Zheng <ljiazh3ng@gmail.com>
2019-07-05 23:02:08 +0800

    Reuploaded JiaZheng's

--- Inescapable/Assets/Scenes/MainMenu.cs
+++ Inescapable/Assets/Scenes/MainMenu.cs
@@ -9,11 +9,7 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
-    public void HighScore()
-    {
-        SceneManager.LoadScene(2);
 
-        }
     public void QuitGame()
     {
         Application.Quit();
