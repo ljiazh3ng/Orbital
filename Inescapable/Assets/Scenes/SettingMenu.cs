bcdd01a6325534b4628f87fbf19ba0f0226e9010
Jia Zheng <ljiazh3ng@gmail.com>
2019-07-08 11:30:09 +0800

    Fixed Option slider problem

---
+++ Inescapable/Assets/Scenes/SettingMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    // Start is called before the first frame update
+    public void AdjustVolume(float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+
+    public void SetQuality(int quality)
+    {
+        QualitySettings.SetQualityLevel(quality);
+    }
+}
