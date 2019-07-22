cadfca830859fbe6e394f3dbe47bb63d6af9dfff
XinWei <zhouxinwei97@gmail.com>
2019-07-22 15:44:16 +0800

    Countdown timer, lose condition, game over overlay
    - Implemented countdown timer of 3minutes.
    
    - Implemented Lose condition
    
    - Implemented the game over overlay to show up when game is lost.

---
+++ Inescapable/Assets/Scenes/Scripts/GameOverOverlay.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOverOverlay : MonoBehaviour
+{
+    public GameObject gameOverUI;
+    public GameObject User;
+    public Text gameOverTextUI; 
+
+    void Update()
+    {
+        bool gameIsOver = TimerGameOver.countDownStartValue == -1;
+        if (gameIsOver)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.lockState = CursorLockMode.Confined;
+            Cursor.visible = true;
+            int keysCollected = ScoringSystem.gameScore;
+            if (keysCollected < 10)
+            {
+                gameOverTextUI.text = "You have collected " + keysCollected + "/10 keys.";
+            } else
+            {
+                gameOverTextUI.text = "You have collected " + keysCollected + "/10 keys but have not made it to the exit.";
+
+            }
+            endGame();
+        }
+    }
+
+    void endGame()
+    {
+        User.SetActive(false);
+        gameOverUI.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Inescapable/Assets/Scenes/Scripts/TimerGameOver.cs
@@ -0,0 +1,39 @@
+﻿using System; 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerGameOver : MonoBehaviour
+{
+
+    public static int countDownStartValue = 5;
+    public Text timerUI; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        countDownTimer();
+
+    }
+
+    void countDownTimer()
+    {
+        if (countDownStartValue >= 0)
+        {
+            TimeSpan spanTime = TimeSpan.FromSeconds(countDownStartValue);            
+            timerUI.text = spanTime.Minutes + ":" + spanTime.Seconds;
+            if (spanTime.Seconds < 10)
+            {
+                timerUI.text = spanTime.Minutes + ":" + "0" + spanTime.Seconds;
+            }
+            countDownStartValue--;
+            Invoke("countDownTimer", 1.0f);
+        }
+        else
+        {
+            timerUI.text = "Game Over!"; 
+        }
+    }
+
+}
