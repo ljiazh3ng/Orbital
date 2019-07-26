46787ac2b82093ac0d7b8a5613432141e7b5324d
XinWei <zhouxinwei97@gmail.com>
2019-07-26 17:37:28 +0800

    Minor tweaks to locations
    -added animation for skeletons

---
+++ Inescapable/Assets/Scenes/Scripts/AnimatorScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorScript : MonoBehaviour
+{
+    public Animator anim;
+
+    void Start()
+    {
+        anim = this.gameObject.GetComponent<Animator>();
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
